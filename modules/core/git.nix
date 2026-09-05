4f95dd23e540746c0c07b04c07c4d2de75e03ba4
Fabien Lassagne <fabien.lassagne@ultra.io>
2026-09-05 15:24:18 +0200

    fix(git): use modern programs.git.settings to eliminate deprecation warnings

--- modules/core/git.nix
+++ modules/core/git.nix
@@ -11,28 +11,28 @@ in {
     programs.git = {
       enable = true;
 
-      aliases = {
-        st = "status";
-        co = "checkout";
-        sw = "switch";
-        br = "branch";
-        ci = "commit";
-        cp = "cherry-pick";
-        unstage = "reset HEAD --";
-        last = "log -1 HEAD";
-        lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      settings = {
+        alias = {
+          st = "status";
+          co = "checkout";
+          sw = "switch";
+          br = "branch";
+          ci = "commit";
+          cp = "cherry-pick";
+          unstage = "reset HEAD --";
+          last = "log -1 HEAD";
+          lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
 
-        # Gestion des branches distantes et des sous-modules
-        clean-branch = "!f() { git fetch --prune; git branch -D $(git branch -vv | grep -E 'disparue|gone' | awk '{print $1}' | xargs); }; f";
-        checkout-all = "!f() { git checkout \"$@\" && git submodule update --init --recursive; }; f";
-        clone-all = "clone --recursive";
-        pull-all = "!f() { git pull \"$@\" && git submodule update --init --recursive; }; f";
-        push-all = "push --recurse-submodules=on-demand";
-        push-master = "push origin master:master";
-        push-tag = "push --tags";
-      };
+          # Gestion des branches distantes et des sous-modules
+          clean-branch = "!f() { git fetch --prune; git branch -D $(git branch -vv | grep -E 'disparue|gone' | awk '{print $1}' | xargs); }; f";
+          checkout-all = "!f() { git checkout \"$@\" && git submodule update --init --recursive; }; f";
+          clone-all = "clone --recursive";
+          pull-all = "!f() { git pull \"$@\" && git submodule update --init --recursive; }; f";
+          push-all = "push --recurse-submodules=on-demand";
+          push-master = "push origin master:master";
+          push-tag = "push --tags";
+        };
 
-      extraConfig = {
         init.defaultBranch = "main";
         rerere = {
           enabled = true;
